dae6140c3a2c358592937a37d791a5f3781b717a
liuxi <liuxiaoxinbb@163.com>
2026-09-04 16:54:08 +0800

    fix: 排除 DataSourceAutoConfiguration，修复多数据源 Flyway 启动完成后的退出报错

--- jeecg-boot/jeecg-module-system/jeecg-module-flyway/src/main/java/org/jeecg/flyway/FlywayApplication.java
+++ jeecg-boot/jeecg-module-system/jeecg-module-flyway/src/main/java/org/jeecg/flyway/FlywayApplication.java
@@ -4,25 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
 /**
  * Flyway 独立数据库版本管理工具启动类
  *
- * <p>使用方式：
+ * <p>支持业务主库 (master) 与 Nacos (nacos) 等多数据源自动检测与增量迁移升级。
+ * <p>运行方式：
  * <pre>
- *   java -jar jeecg-module-flyway.jar \
- *     --spring.datasource.url=jdbc:mysql://localhost:3306/jeecg-boot?... \
- *     --spring.datasource.username=root \
- *     --spring.datasource.password=root \
- *     --spring.flyway.enabled=true
+ *   java -jar jeecg-module-flyway.jar
  * </pre>
  *
- * <p>启动后自动执行 flyway/sql/mysql/ 下的增量 SQL，完成后进程自动退出。
+ * <p>启动后自动完成所有数据源的校验与升级，完成后进程自动安全退出（退出码 0）。
  *
  * @author jeecg
  */
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
 public class FlywayApplication {
 
     public static void main(String[] args) {
@@ -30,6 +32,6 @@ public class FlywayApplication {
         // 非 Web 模式：迁移完成后自动退出，无需手动关闭
         app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
-        log.info("【数据库升级】Flyway 迁移工具执行完毕，进程退出。");
+        log.info("【数据库升级】Flyway 迁移工具执行完毕，进程安全退出。");
     }
 }
